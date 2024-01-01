9535859aeead09adfc9c93aa8757e95ee80df3fa
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2024-01-01 13:29:12 -0300

    Acerto caracteres inválidos no DML

--- DML.sql
+++ DML.sql
@@ -7,7 +7,7 @@ INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('MARCOS VINICIO'
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('MARIA MARIA MARIA', 'maria@terra.com.br', '11b0a203e7cef718acb4eb1bf01dd696', '2023-12-27 08:02:53');
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('CRISTINA CRISTINA', 'cristina@teste.com.br', '15e3d4cd28db023f88967a4e3ec284a7', '2023-12-27 08:03:29');
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('ZULEIKA ZULEIKA', 'zuleika@teste.com.br', 'c0e259d7674b2f763fe1eb722f83a087', '2023-12-27 15:48:20');
-INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('FABRÍCIO FABRÍCIO', 'fabricio@teste.com.br', 'b179724156c8e14cc169c45191e07561', '2023-12-27 17:05:38');
+INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('FABRÍCIO FABRÍCIO', 'fabricio@teste.com.br', 'b179724156c8e14cc169c45191e07561', '2023-12-27 17:05:38');
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('HEITOR HEITOR', 'heitor@teste.com.br', '307623f4dd06a01d6d30f8881688fb59', '2023-12-27 17:07:37');
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('NOEL NOEL', 'noel@teste.com.br', 'cd663d425ead87b8089ea4d9e7854292', '2023-12-27 17:26:43');
 INSERT INTO usuarios (nome, email, senha, data_criacao) VALUES ('XAVIER XAVIER', 'xavier@teste.com.br', '0c6467d74d41476a58d40fe9502ce8fd', '2023-12-27 18:51:58');
